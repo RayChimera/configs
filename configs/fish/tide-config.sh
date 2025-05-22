ed44fe4fa2d621d0c53e643bb25ad429e9fded6f
Alice Yudina <chimera@chimera.website>
2025-05-22 04:20:47 +0300

    Made Tide configuration a separate file

---
+++ configs/fish/tide-config.sh
@@ -0,0 +1,2 @@
+#!/bin/fish
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Sparse --icons='Many icons' --transient=No
